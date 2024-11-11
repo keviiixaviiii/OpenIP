5ae9df329498744890b4bb4eb42395079ec62d88
unknown <iamkevinxavi@gmail.com>
2024-11-11 20:33:13 +0000

    App commit

---
+++ metro.config.js
@@ -0,0 +1,13 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/metro
+ *
+ * @format
+ */
+
+module.exports = {
+    resolver: {
+      sourceExts: ['jsx', 'js', 'ts', 'tsx'], // Add your file extensions here
+    },
+  };
+  
